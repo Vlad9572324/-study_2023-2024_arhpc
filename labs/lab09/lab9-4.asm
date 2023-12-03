00b5a1bc70f5c1a5bc2a9714d9e688548ab868fe
Vlad <vladostanin0@gmail.com>
2023-12-03 16:38:44 +0300

    add 9 lab

---
+++ labs/lab09/lab9-4.asm
@@ -0,0 +1,38 @@
+%include 'in_out.asm'
+SECTION .data
+msg db "Результат: ",0
+fx: db 'f(x)=2x+15 ',0
+
+SECTION .text
+global _start
+_start:
+mov eax, fx
+call sprintLF
+pop ecx 
+pop edx
+sub ecx,1
+mov esi, 0
+
+next:
+cmp ecx,0h
+jz _end 
+pop eax
+call atoi
+call calc
+add esi,eax
+
+loop next
+
+_end:
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+call quit
+
+calc:
+mov ebx,2 
+mul ebx
+add eax,15
+ret
+
